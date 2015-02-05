0223a7c684f36a04fe79ffe8f73454a903a1fef8
arturograu <argrau3@gmail.com>
2015-02-05 14:30:39 +0100

    Comenzando con el código
    Signed-off-by: arturograu <argrau3@gmail.com>

---
+++ PruebaSQLite/src/Conectar.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * Clase encargada de conectar a la Base de Datos
+ */
+public final class Conectar {
+	
+	/**
+	 * Conecta a una base de datos en la ruta pasada como argumento
+	 * Si el archivo señalado en la ruta no existe, se crea
+	 * @param ruta - La ruta de la DB a conectar
+	 * @return - La conexión a la ruta
+	 */
+	private static Connection conectarA(String ruta){
+		
+	}
+
+}
