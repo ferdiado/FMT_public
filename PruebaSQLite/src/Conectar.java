3e15b3602dfdf78c7034bf4c39accb61d757042f
arturograu <argrau3@gmail.com>
2015-02-06 20:43:09 +0100

    La aplicación ya se conecta a la BD
    Signed-off-by: arturograu <argrau3@gmail.com>

--- PruebaSQLite/src/Conectar.java
+++ PruebaSQLite/src/Conectar.java
@@ -13,8 +13,31 @@ public final class Conectar {
 	 * @param ruta - La ruta de la DB a conectar
 	 * @return - La conexión a la ruta
 	 */
-	private static Connection conectarA(String ruta){
+	public static Connection conectarA(String ruta){
+		try {
+			//Cargamos el driver de SQLite
+			Class.forName("org.sqlite.JDBC");
+		}
+		catch (ClassNotFoundException e) {
+			//Esto se ejecuta si hay un error con el driver de la BD
+			e.printStackTrace();
+		}
 		
+		//Declaramos la conexión:
+		Connection conn = null;
+		
+		try {
+			//Aquí se obtiene la conexión
+			conn = DriverManager.getConnection("jdbc:sqlite:" + ruta);
+			//Un mensaje en la consola para saber si se ha realizado la conexión
+			System.out.println("Conexión realizada con éxito - Ruta de la Base de Datos:" + ruta);
+			}
+		catch (SQLException e) {
+			//Esto se ejecuta si hay un error en la Base de Datos
+			e.printStackTrace();
+		}
+		
+		//Devolvemos la conexión
+		return conn;
 	}
-
 }
